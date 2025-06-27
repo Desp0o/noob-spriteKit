f1a021baa011fd7b21f6979e5925f5dbc31a6c67
desp0o <tornike.despotashvili@gmail.com>
2025-06-27 23:30:46 +0400

    add first spritenode in scene

--- noob-spriteKit/GameScene.swift
+++ noob-spriteKit/GameScene.swift
@@ -10,5 +10,11 @@ import SpriteKit
 final class GaameScene: SKScene {
   override func didMove(to view: SKView) {
     print("hello")
+    let square = SKSpriteNode()
+    square.size = CGSize(width: 20, height: 20)
+    square.color = .systemPink
+    square.position = CGPoint(x: 100, y: 100)
+    
+    addChild(square)
   }
 }
