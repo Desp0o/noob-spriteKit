6579c9bf9eadd6b4bd9ff311011f1fff317dfe10
desp0o <tornike.despotashvili@gmail.com>
2025-06-27 23:38:12 +0400

    update sprite position with touch

--- noob-spriteKit/GameScene.swift
+++ noob-spriteKit/GameScene.swift
@@ -8,13 +8,19 @@
 import SpriteKit
 
 final class GaameScene: SKScene {
+  let square = SKSpriteNode()
+  
   override func didMove(to view: SKView) {
-    print("hello")
-    let square = SKSpriteNode()
     square.size = CGSize(width: 20, height: 20)
     square.color = .systemPink
     square.position = CGPoint(x: 100, y: 100)
     
     addChild(square)
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    for touch in touches {
+      square.position = touch.location(in: self)
+    }
   }
 }
